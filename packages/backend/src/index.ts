aca03381f519f0493012e0493858f28a9c8d3a49
Gibson Smith <123333011+gdsmith1@users.noreply.github.com>
2024-11-20 08:20:53 -0800

    techdocs + first template

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -56,4 +56,6 @@ backend.add(import('@backstage/plugin-search-backend-module-techdocs/alpha'));
 // kubernetes
 backend.add(import('@backstage/plugin-kubernetes-backend/alpha'));
 
-backend.start();
+backend.add(import('@backstage/plugin-scaffolder-backend-module-github'));
+  
+  backend.start();
